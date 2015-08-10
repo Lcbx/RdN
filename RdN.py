07f9e4a9b8296edb10bb4ef1f580dfa190d6b911
Lcbx <luc.courbariaux@gmail.com>
2015-08-10 11:51:34 -0400

    multi-layers non-fonctionnal

--- RdN.py
+++ RdN.py
@@ -16,39 +16,52 @@ valid_set_x, valid_set_y = valid_set
 # plt.imshow(image, cmap = cm.Greys_r)
 # plt.show()
 
-batch = 10
+
 #the RdN
 class layer(object):
-    def __init__(self): # Notre methode constructeur        
-        self.w = np.zeros((784,10))
-        self.b = np.zeros((1,10))
+    def __init__(self, nIn, nOut): # Notre methode constructeur        
+        self.w = np.random.random_integers(-3,3, (nIn,nOut))
+        self.b = np.random.random_integers(-3,3, (1,nOut))
     def fprop(self, x):
     	z = np.dot(x, self.w) + self.b
     	return z
-    def update(self, lr, x, t):
-    	dEdz = 2*(self.fprop(x) - np.eye(10)[t])
-    	dEdw = np.dot(x.T, dEdz)
-    	dEdb = np.mean(dEdz, axis = 0)    	
+    def update(self, lr, x, dEdy):
+    	dEdw = np.dot(x.T, dEdy)
+    	dEdb = np.mean(dEdy, axis = 0)    	
     	self.w = self.w - dEdw * lr
     	self.b = self.b - dEdb * lr
-    	return
+    	dEdx = np.dot(dEdy,self.w.T)
+    	return dEdx
+    
+class MLP(object):
+    def __init__(self):
+        self.L = layer(784,100)
+        self.M = layer(100,10)
+    def fprop(self, x):
+        self.cache = self.L.fprop(x)
+        return self.M.fprop(self.cache)
+    def update(self, lr, x, t):
+        dEdy = 2*(self.fprop(x) - np.eye(10)[t])
+        self.L.update(lr, x, self.M.update(lr, self.cache, dEdy))
+        return
     def error(self, x, t):
         z = np.argmax(self.fprop(x), axis = 1)
         errors = np.sum(z!=t)
         return errors
 
-# layer 1
-L = layer()
+     
+RdN = MLP()
+batch = 1000 # nbr dexemples appris a la fois
 # mesuring time
 start = time.time()
-for i in range(5):
+for i in range(6):
     # training
     for j in range(50000/batch):
-    	L.update(.0001, train_set_x[j*batch:(j+1)*batch], train_set_y[j*batch:(j+1)*batch])
+    	RdN.update(.00001, train_set_x[j*batch:(j+1)*batch], train_set_y[j*batch:(j+1)*batch])
     # mesuring error rate
     error_count = 0
     for j in range(10000/batch):
-        error_count += L.error(valid_set_x[j*batch:(j+1)*batch], valid_set_y[j*batch:(j+1)*batch])
+        error_count += RdN.error(valid_set_x[j*batch:(j+1)*batch], valid_set_y[j*batch:(j+1)*batch])
     error_rate = error_count / 10000.
     # report
     print "validation error rate : " + str(error_rate) + " epoch " + str(i) 
